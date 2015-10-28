d8d8f364d0ef3bfd60cc67b4bee7c8c657fdd84b
Pavel Berkovich <pberkovich1994@gmail.com>
2015-10-28 19:17:26 +0000

    Length of LCS

---
+++ II/BioInf/lcs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# Longest Common Subsequence using dynamic programming
+import pprint, time, os
+
+pp = pprint.PrettyPrinter()
+
+
+if __name__ == "__main__":
+    s1 = "tgcata"
+    s2 = "atctgat"
+    
+    l1 = len(s1)
+    l2 = len(s2)
+    
+    mtx = [[0 for x in range(l2 + 1)] for x in range(l1 + 1)] 
+    
+    for i in range(1, l1 + 1):
+        for j in range(1, l2 + 1):
+            mtx[i][j] = max(mtx[i-1][j], mtx[i][j-1], mtx[i-1][j-1] + 1 if s1[i-1] == s2[j-1] else 0)
+            os.system("clear")
+            pp.pprint(mtx)
+            time.sleep(0.5)
+            
+            
+    print "The longest common subsequence of %s and %s has length %d" % (s1, s1, mtx[l1][l2])
+            
+    
+            
