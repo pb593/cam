e3880c2ef98f82574ceb5c82d106fd67bf20f552
Pavel Berkovich <pberkovich1994@gmail.com>
2015-11-21 13:45:17 +0000

    Added DSP folder. Did ex5.

---
+++ II/dsp/sup1/ex5.m
@@ -0,0 +1,18 @@
+b = [1, 0, 0, 2, 2];
+
+% First, make up the matrix A and multiply it by vector b
+a = [-3, 2, 1];
+r = [a(3), 0, 0, 0, 0];
+c = [fliplr(a), 0, 0, 0, 0];
+A = toeplitz(c, r)
+
+rst1 = transpose(mtimes(A, transpose(b)))
+% Now, let's do it using the conv func
+rst2 = conv(fliplr(a), b)
+
+
+
+
+
+
+
