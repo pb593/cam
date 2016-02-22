e5796f5394afc26fa9fd14e41d3ff3d880804a5b
Pavel Berkovich <pberkovich1994@gmail.com>
2016-02-22 23:10:40 +0000

    Extended Euclid implemented

--- II/security/euclid.py
+++ II/security/euclid.py
@@ -10,5 +10,23 @@ def gcd(a, b):
 
 	return a
 
+def egcd(a, b):
+	if a < b: # ensure a >= b to begin with
+		(a, b) = (b, a)
+
+	(a0, b0) = (a, b)
+
+	(aa, ab) = (1, 0)
+	(ba, bb) = (0, 1)
+
+	while(True):
+		q = a / b
+
+		if(a == q * b):
+			return (b, ba, bb)
+		else:
+			(a, aa, ab, b, ba, bb) = (b, ba, bb, a - b*q, aa - q*ba ,ab - q*bb)
+
+
 if __name__ == "__main__":
-	print(gcd(36, 24))
+	print(egcd(733810016255931844845,1187329547587210582322))
