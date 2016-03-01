737e6b62594aea656d481dbfbe4d71375c2c499d
Pavel Berkovich <pberkovich1994@gmail.com>
2016-03-01 21:09:39 +0000

    Did 16.E in from Sec2.4 work.

--- II/security/dsa.py
+++ II/security/dsa.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-
+import hashlib
 from modexp import modexp
-from euclid import modinv
 from cryptomath import findModInverse
 
 P = 0x8df2a494492276aa3d25759bb06869cbeac0d83afb8d0cf7cbb8324f0d7882e5d0762fc5b7210eafc2e9adac32ab7aac49693dfbf83724c2ec0736ee31c80291
@@ -22,23 +21,57 @@ Sigs = [(0x932eeb1076c85e522f02e15441fa371e3fd000ac,
          0x25f87cbb380eb4d7244963e65b76677bc968297e)]
 
 
+def sha1(input):
+    return hashlib.sha1(input).hexdigest()
+
 def F(x):
     return x % Q
+    
+def verify(h, r, s):   
+    sInv = findModInverse(s, Q)
+    vrf = F(pow(G, h*sInv, P) * pow(Y, r*sInv, P) % P)
+    
+    return (r == vrf)
 
 if __name__ == "__main__":
     
+    print("Part B\n")
     for h, r, s in Sigs:
         print("Message hash: %s" % h)
         print("r = %s" % r)
         print("s = %s" % s)
         
-        sInv = findModInverse(s, Q)
-        vrf = F(pow(G, h*sInv, P) * pow(Y, r*sInv, P) % P)
-        
-        print("Vrf: %s" % vrf)
-        if(r == vrf):
+        if(verify(h, r, s)):
             print("Signature verified")
         else:
             print("Signature verification failed")
+        print("-------------------------------------------------------------")
+            
+            
+    print("\nPart E\n")
+    h1 = Sigs[0][0]
+    h2 = Sigs[1][0]
+    s1 = Sigs[0][2]
+    s2 = Sigs[1][2]
+    r = Sigs[0][1]
+    
+    invDeltaS = findModInverse(s1-s2, Q)
+    k = (h1 - h2) * invDeltaS % Q
+    kInv = findModInverse(k, Q)
+    
+    rInv = findModInverse(r, Q)
+    # secret key
+    x = (k * s1 - h1) * rInv % Q
+    
+    h3 = int(sha1("Wednesday"), 16)
+    print(h3)
+    sig = ( kInv * (h3 + x * r) ) % Q
+    
+    if(verify(h3, r, sig)):
+        print("Fake signature verified!")
+    else:
+        print("Fake signature failed to verify. Try again...")
+    
+     
         
         
